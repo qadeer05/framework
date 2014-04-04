6a0c02d163fa8a153aea758ae3834ab7ddac56f8
Malte <scharenberg91@googlemail.com>
2014-04-04 13:37:13 +0200

    Added BooleanTest

---
+++ src/Component/Filter/Tests/BooleanTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pagekit\Component\Filter\Tests;
+
+use Pagekit\Component\Filter\Boolean;
+
+class BooleanTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFilter()
+    {
+        $filter = new Boolean;
+
+        $values = array(
+            0   => false,
+            ""  => false,
+            "1" => true
+
+        );
+        foreach ($values as $in => $out) {
+            $this->assertSame($filter->filter($in), $out);
+        }
+
+    }
+
+}
